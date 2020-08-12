ae40cee71dff88fe1a87cca58b376cc29c73f57a
Erick Martinez <erickm@auctionedge.com>
2020-08-12 18:50:45 -0500

    Support multiple carriers

--- app/jobs/track_job.rb
+++ app/jobs/track_job.rb
@@ -2,16 +2,22 @@
 
 class TrackJob < ApplicationJob
 
+  CARRIERS = [
+    'Fedex'
+  ].freeze
+
   after_perform { TrackerResponder.send_results(@track_id, @results) }
 
   def perform(track_id, tracking_info)
     @track_id = track_id
     @results = []
 
-    tracking_info_for_carrier = tracking_info.select { |info| info[:carrier]&.casecmp?('FEDEX') }
-    tracking_info = tracking_info - tracking_info_for_carrier
-    tracking_numbers = tracking_info_for_carrier.map { |info| info[:tracking_number] }
-    @results.push(*TrackFromFedex.track(tracking_numbers))
+    CARRIERS.each do |carrier|
+      tracking_info_for_carrier = tracking_info.select { |info| info[:carrier]&.casecmp?(carrier) }
+      tracking_info = tracking_info - tracking_info_for_carrier
+      tracking_numbers = tracking_info_for_carrier.map { |info| info[:tracking_number] }
+      @results.push(*"TrackFrom#{carrier}".constantize.public_send(:track, tracking_numbers))
+    end
 
     @results.push(*tracking_info.map { |info| unknown_carrier_response(info[:tracking_number]) })
 
